23eb8e5a63234ce2961139d2776c863958cd7b2e
Justin Tulloss <jmtulloss@gmail.com>
2009-01-22 09:51:16 -0800

    Created an install method so that cobra may be installed into the global namespace. The default is to keep it in its own, Cobra namespace

--- src/core.js
+++ src/core.js
@@ -8,3 +8,15 @@ Cobra.$A = function(iterable) {
     return Array.prototype.slice.call(iterable);
 }
 
+/* This function makes all the top level cobra objects part of
+ * the global namespace. Do this if cobra is the exclusive library
+ * you are using to provide class functionality.
+ */
+Cobra.install = function() {
+    var key;
+    for (key in Cobra) {
+        if (key != 'install') {
+            window[key] = Cobra[key];
+        }
+    }
+}
