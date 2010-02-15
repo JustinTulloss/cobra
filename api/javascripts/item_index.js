c20bcfce2855af3f4b015a24e5bf26866f15b227
Justin Tulloss <jmtulloss@gmail.com>
2010-02-15 02:57:26 -0800

    Added generated API docs

---
+++ api/javascripts/item_index.js
@@ -0,0 +1,13 @@
+if (!window.PDoc) window.PDoc = {};
+PDoc.elements = {
+  'Cobra section': { 'name': 'Cobra section', 'type': 'section', 'path': 'cobra/' },
+'Cobra': { 'name': 'Cobra', 'type': 'namespace', 'path': 'cobra/cobra/' },
+'Cobra.config': { 'name': 'Cobra.config', 'type': 'class property', 'path': 'cobra/cobra/config/' },
+'Cobra.install': { 'name': 'Cobra.install', 'type': 'class method', 'path': 'cobra/cobra/install/' },
+'Cobra.Class': { 'name': 'Cobra.Class', 'type': 'class', 'path': 'cobra/cobra/class/' },
+'new Cobra.Class': { 'name': 'new Cobra.Class', 'type': 'constructor', 'path': 'cobra/cobra/class/new/' },
+'Cobra.Class.method': { 'name': 'Cobra.Class.method', 'type': 'class method', 'path': 'cobra/cobra/class/method/' },
+'Cobra.Class.ancestor': { 'name': 'Cobra.Class.ancestor', 'type': 'class method', 'path': 'cobra/cobra/class/ancestor/' },
+'Cobra.Singleton': { 'name': 'Cobra.Singleton', 'type': 'class', 'path': 'cobra/cobra/singleton/' },
+'new Cobra.Singleton': { 'name': 'new Cobra.Singleton', 'type': 'constructor', 'path': 'cobra/cobra/singleton/new/' }
+};
